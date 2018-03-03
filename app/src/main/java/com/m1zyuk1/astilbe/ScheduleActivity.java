b94eb232920694e52f17d8c93564c2cb07f0487b
mizune <willful.slight@gmail.com>
2018-03-03 14:29:29 +0900

    Rename activity

--- app/src/main/java/com/m1zyuk1/astilbe/ScheduleActivity.java
+++ app/src/main/java/com/m1zyuk1/astilbe/ScheduleActivity.java
@@ -3,11 +3,11 @@ package com.m1zyuk1.astilbe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateScheduleActivity extends AppCompatActivity {
+public class ScheduleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_schedule);
+        setContentView(R.layout.activity_schedule);
     }
 }
