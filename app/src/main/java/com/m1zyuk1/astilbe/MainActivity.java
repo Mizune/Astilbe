da9f0c52e5a3c59cf9863ff0802f8afd1fa2b800
mizune <willful.slight@gmail.com>
2018-03-03 09:36:24 +0900

    Enable lambda

--- app/src/main/java/com/m1zyuk1/astilbe/MainActivity.java
+++ app/src/main/java/com/m1zyuk1/astilbe/MainActivity.java
@@ -6,18 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List schedules = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUi();
+    }
+
+    // TODO recyclerViewを持たせる
+    // TODO DataBinding使えるようにする
+    // TODO とりあえずStringデータが表示できるようにする
+    //
+
+    private void initializeSchedules(){
+        // pref見て binding
+    }
+
+    private void setupUi(){
+        // binding追加してonClickとか titleとか dialog初回だしとか
+        setupFloatingActionButton();
+    }
+
+    private void setupFloatingActionButton(){
         FloatingActionButton fab = findViewById(R.id.add_schedule_button);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
-            }
+        fab.setOnClickListener((view) -> {
+            Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
         });
     }
+
 }
