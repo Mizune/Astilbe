c97bb56abfb4acc3c5de08af7ae78da65424d7a6
mizune <willful.slight@gmail.com>
2018-03-03 14:26:50 +0900

    Create new Activities

---
+++ app/src/main/java/com/m1zyuk1/astilbe/CreateScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.m1zyuk1.astilbe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_schedule);
+    }
+}
